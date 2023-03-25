0d344eeef16e7e2734af1d6eb0c3757ecad2d1bf
pingping0904 <122675205+pingping0904@users.noreply.github.com>
2023-03-25 07:15:20 -0700

    Update index.blade.php
    success index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,7 +10,7 @@
                     <i class="fas fa-home"></i> @lang('navs.general.home')
                 </div>
                 <div class="card-body">
-                    @lang('strings.frontend.welcome_to', ['place' => app_nam()])
+                    @lang('strings.frontend.welcome_to', ['place' => app_name()])
                 </div>
             </div><!--card-->
         </div><!--col-->
